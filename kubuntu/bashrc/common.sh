f0582d60b5c053c6b2903fa23fed9b988075d32a
Tyler Dalton <tldalton54@gmail.com>
2026-09-22 19:35:58 -0400

    Create common.sh

---
+++ kubuntu/bashrc/common.sh
@@ -0,0 +1,19 @@
+# =========================
+# Shell / Navigation
+# =========================
+
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+
+alias home='cd ~'
+alias c='clear'
+alias hist='history'
+
+alias ll='ls -alF --color=auto --group-directories-first'
+alias la='ls -A --color=auto --group-directories-first'
+alias l='ls -CF --color=auto --group-directories-first'
+
+alias reload='source ~/.bashrc'
